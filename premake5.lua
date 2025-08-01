0aa70541d0b0fabf53d626f7a2b7ad0e468aa68c
Htrap19 <parthl@ivalueplus.com>
2025-08-01 16:41:42 +0530

    Add premake build system

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+project "spdlog"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "%{prj.name}")
+	objdir ("%{wks.location}/obj/" .. outputdir .. "%{prj.name}")
+
+	includedirs
+	{
+		"include"
+	}
+
+	files
+	{
+		"src/**.cpp",
+		"src/**.h",
+		"include/**.cpp",
+		"include/**.h",
+	}
+
+	defines
+	{
+		"SPDLOG_COMPILED_LIB"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+		buildoptions { "/utf-8" }
+
+	filter { "system:windows", "configurations:Debug" }
+		runtime "Debug"
+		symbols "on"
+
+	filter { "system:windows", "configurations:Release" }
+		runtime "Release"
+		optimize "on"
+		buildoptions "/MT"
